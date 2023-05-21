da31b6fef083d39489ef62adc41385db6d4e8f41
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-05-21 02:22:31 +0100

    worked on database connection

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,11 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  host: "localhost",
-  user: "postgres",
-  password: "Dsquare142",
-  database: "sidebrief",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 client.connect((error, response) => {
