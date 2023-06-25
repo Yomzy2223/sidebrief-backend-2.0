ccaeb33c02c65f5da88f9825546e2889f0b96d4b
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-06-25 09:55:38 +0100

    validator mdidleware

--- src/tests/bank.test.js
+++ src/tests/bank.test.js
@@ -4,12 +4,35 @@ const app = require("../../app");
 const prisma = new PrismaClient();
 
 describe("Testing all bank endpoints", () => {
+  let staffToken = "";
+  let userToken = "";
+
   beforeAll(async () => {
     await prisma.bank.delete({
       where: {
         bankName: "test",
       },
     });
+
+    const loginData = {
+      email: process.env.TEST_STAFF,
+      password: process.env.TEST_STAFF_PASSWORD,
+    };
+
+    const StaffRes = await request(app).post("/staffs/login").send(loginData);
+    expect(StaffRes.body.message).toBe("Login successfully");
+    staffToken = StaffRes.body.data.token;
+
+    const userLoginData = {
+      email: process.env.TEST_USER,
+      password: process.env.TEST_USER_PASSWORD,
+    };
+
+    const userResponse = await request(app)
+      .post("/users/login")
+      .send(userLoginData);
+    expect(userResponse.body.message).toBe("Login successfully");
+    userToken = userResponse.body.data.token;
   });
 
   const data = {
@@ -20,7 +43,10 @@ describe("Testing all bank endpoints", () => {
   };
 
   it("should create a new bank", async () => {
-    const res = await request(app).post("/banks").send(data);
+    const res = await request(app)
+      .post("/banks")
+      .set("Authorization", `Bearer ${staffToken}`)
+      .send(data);
     expect(res.statusCode).toBe(200);
     expect(res.body.message).toBe("Bank created successfully");
     expect(res.body.data.bankName).toBe("test");
@@ -34,19 +60,24 @@ describe("Testing all bank endpoints", () => {
 
   const bankId = "87c0f77d-1cbc-4583-8edf-355987d20871";
   it("should return a 200", async () => {
-    const res = await request(app).get(`/banks/${bankId}`);
+    const res = await request(app)
+      .get(`/banks/${bankId}`)
+      .set("Authorization", `Bearer ${userToken}`);
     expect(res.statusCode).toBe(200);
   });
 
   const w_bankId = "8aa2882e-78bd-4c88-9968-474bbc6a6545";
   it("should return a 400", async () => {
-    const res = await request(app).get(`/banks/${w_bankId}`);
+    const res = await request(app)
+      .get(`/banks/${w_bankId}`)
+      .set("Authorization", `Bearer ${userToken}`);
     expect(res.statusCode).toBe(400);
   });
 
   it("should update a bank", async () => {
     const res = await request(app)
       .put("/banks/87c0f77d-1cbc-4583-8edf-355987d20871")
+      .set("Authorization", `Bearer ${staffToken}`)
       .send({
         bankName: "Sterling Bank",
         bankCode: "23333",
@@ -60,6 +91,7 @@ describe("Testing all bank endpoints", () => {
   it("should return 400 ", async () => {
     const res = await request(app)
       .put("/banks/dbc5b4f4-5b81-46f6-a1b4-2bea6646a45c")
+      .set("Authorization", `Bearer ${staffToken}`)
       .send({
         bankName: "test",
         bankCode: "this is a test bank",
