8d24a321a063745da355fa82de523a448dfd4924
Akinyemi Bamidele <59539644+bamidele2161@users.noreply.github.com>
2023-11-15 10:47:30 +0100

    sign up with google

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -22,33 +22,33 @@ const router = express.Router();
 import passport from "passport";
 import { staffAuth, userAuth } from "../../middleware/auth";
 // OAuth routes
-// router.get(
-//   "/auth/google",
-//   passport.authenticate("google-signup", { scope: ["profile", "email"] })
-// );
+router.get(
+  "/auth/google",
+  passport.authenticate("google-signup", { scope: ["profile", "email"] })
+);
 
-// router.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/auth" }),
-//   (req, res) => {
-//     // Redirect or perform any additional logic after successful authentication
-//     res.status(200).json({ data: req.user });
-//   }
-// );
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth" }),
+  (req, res) => {
+    // Redirect or perform any additional logic after successful authentication
+    res.status(200).json({ data: req.user });
+  }
+);
 
-// router.get(
-//   "/auth/google/signin",
-//   passport.authenticate("google-signin", { scope: ["profile", "email"] })
-// );
+router.get(
+  "/auth/google/signin",
+  passport.authenticate("google-signin", { scope: ["profile", "email"] })
+);
 
-// router.get(
-//   "/auth/login/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/auth" }),
-//   (req, res) => {
-//     // Redirect or perform any additional logic after successful authentication
-//     res.status(200).json({ data: req.user });
-//   }
-// );
+router.get(
+  "/auth/login/google/callback",
+  passport.authenticate("google", { failureRedirect: "/auth" }),
+  (req, res) => {
+    // Redirect or perform any additional logic after successful authentication
+    res.status(200).json({ data: req.user });
+  }
+);
 
 router.post("/", validator(validateUser), UserRegisration);
 router.post("/login", validator(validateUserCredentials), UserGrantor);
